dee96e2708a26ba2145d8493e7ffd47f52ac5f84
Masa-1994 <kyani_m1994@icloud.com>
2019-12-12 21:30:35 +0900

    postルーティング設定

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  #投稿一覧ページ
+  root 'posts#index'
 end
