5db8b6c7be79af7a441651d3b4561a7eb98abceb
Masa-1994 <kyani_m1994@icloud.com>
2019-12-19 17:28:00 +0900

    アクティブハッシュ

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,9 +1,11 @@
 class User < ApplicationRecord
+  extend ActiveHash::Associations::ActiveRecordExtensions
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise  :database_authenticatable, :registerable,
           :recoverable, :rememberable, :validatable
 
   # associate
-  has_many :posts
+  has_many               :posts
+  belongs_to_active_hash :prefecture
 end
