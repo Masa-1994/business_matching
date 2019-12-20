325599879f8633f3bfcaaac72ac7bd4be63dc54c
Masa-1994 <kyani_m1994@icloud.com>
2019-12-21 04:48:01 +0900

    postコントローラー

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,4 +1,18 @@
 class PostsController < ApplicationController
   def index
+    @posts = Post.all
+  end
+
+  def new
+    @post = Post.new
+  end
+
+  def create
+    Post.create(title: post_params[:title], text: post_params[:text], business: post_params[:business], area: post_params[:area], image: post_params[:image], user_id: current_user.id)
+  end
+
+  private
+  def post_params
+    params.permit(:title, :text, :business, :area, :image,)
   end
 end
